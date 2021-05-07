46dcc3700ee17e0a25bd4a3c3512b01b086ff6c8
Alan Goodier <alangoodier@gmail.com>
2021-05-07 10:43:10 +0100

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './components/sidebar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+    <Sidebar width={300} height={"100vh"}>
+          <h3>Welcome to Kard!</h3>
+          <h3>Alan G</h3>
+          <br></br>
+          <button className="boards-button" type="submit">My Boards</button>
+          <h2>New Project</h2>
+          <h2>Logout</h2>
+        </Sidebar>
+      </div>
     </div>
   );
 }
